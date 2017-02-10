4c206bd22a1936171bbcd25a101870e848e5ff09
Joakim Erdfelt <joakim@webtide.com>
2017-02-10 15:47:03 -0700

    dns update
    Signed-off-by: Joakim Erdfelt <joakim@webtide.com>

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker run -it --rm \
-     --dns=192.168.1.1 \
-     --dns-search=phx.erdfelt.net \
+     --dns=10.0.0.1 \
+     --dns-search=piratecode.org \
      --volume /usr/bin/docker:/usr/bin/docker \
      --volume /var/run/docker.sock:/var/run/docker.sock \
      --volume /opt/shared:/opt/shared \
