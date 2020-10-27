226cbd7defe846049b5b55c7dd6d9c1affe42112
olivier lamy <olamy@apache.org>
2020-10-27 12:07:51 +1000

    add 18:04 image with default jdk8
    Signed-off-by: olivier lamy <olamy@apache.org>

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 #not used anymore
 #docker build --no-cache --tag=jenkins-slave:latest docker/
 
-docker build --no-cache --tag=jetty-build:latest build-image/
+docker build --no-cache --tag=jetty-build-18-04-jdk8:latest build-image/
 
 #to deploy
-# docker tag jetty-build:latest jettyproject/jetty-build:latest
-# docker push jettyproject/jetty-build:latest
+# docker tag jetty-build-18-04-jdk8:latest jettyproject/jetty-build-18-04-jdk8:latest
+# docker push jettyproject/jetty-build-18-04-jdk8:latest
 
