29edcb671304e6a0aa74a8f1571b149136c19c4d
Olivier Lamy <olamy@apache.org>
2022-11-09 08:17:02 +1000

    fix script
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@
 #not used anymore
 #docker build --no-cache --tag=jenkins-slave:latest docker/
 
-docker build --no-cache --tag=jetty-build:latest build-image/
+#docker build --no-cache --tag=jetty-build:latest build-image/
+
+docker build --no-cache --tag=jetty-build-agent:latest slave-image/
 
 #to deploy
-# docker tag jetty-build:latest jettyproject/jetty-build:latest
-# docker push jettyproject/jetty-build:latest
 
+docker tag jetty-build-agent:latest jettyproject/jetty-build-agent:$timestamp
+docker push jettyproject/jetty-build-agent:$timestamp
