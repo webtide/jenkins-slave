768957b380e68afbad49e56fb922ecf04996e237
olamy@webtide.com <Olivier Lamy>
2024-06-21 05:48:36 +0000

    push changes
    Signed-off-by: olamy@webtide.com <Olivier Lamy>

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@
 
 #docker build --no-cache --tag=jetty-build:latest build-image/
 
+#docker build --no-cache --tag=jetty-build-agent:latest slave-image/
 docker build --tag=jetty-build-agent:latest slave-image/
 
-docker build --no-cache --tag=jetty-build-agent:latest slave-image/
-
 #to deploy
-
-docker tag jetty-build-agent:latest jettyproject/jetty-build-agent:$timestamp
-docker push jettyproject/jetty-build-agent:$timestamp
+#timestamp format: 11-09-2023-16-31
+export TS=`date +%d-%m-%Y-%H-%M`
+docker tag jetty-build-agent:latest jettyproject/jetty-build-agent:$TS
+docker push jettyproject/jetty-build-agent:$TS
+echo $TS
